078e50e3f629abfd760dc13266eefb3ee53b1530
Danil <bion-1@mail.ru>
2024-01-03 01:12:57 +0100

    com9

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            <input
+                className="border-2"
+                type="text"
+            />
+
+        </div>
+    );
+};
+
+export default Main;
