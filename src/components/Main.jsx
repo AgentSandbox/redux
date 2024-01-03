e68f0d5982de886cf862e60daf80f4d310586a07
AgentDan <pregrada@mail.com>
2024-01-03 02:02:51 +0100

    com10

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {setData} from "../features/dataSlice.jsx";
 
 const Main = () => {
+    const dispatch = useDispatch()
+    const name = useSelector((state) => state.data.data)
     return (
         <div>
             <input
                 className="border-2"
+                value={name}
+                onChange={(e)=>{dispatch(setData(e.target.value))}}
                 type="text"
             />
+            <div>
+                {name}
+            </div>
 
         </div>
     );
